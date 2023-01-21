bc134a9c469610272b789ed5888fc61132ea47cf
Adam Berg <adam@xyzdigital.com>
2023-01-21 11:02:46 -0800

    exclude some paths from the generated tar

--- index.mjs
+++ index.mjs
@@ -1,32 +1,75 @@
 #!/usr/bin/env node
 import fs from "fs";
+import fsPromise from "fs/promises";
+import axios from "axios";
+import FormData from "form-data";
+import tar from "tar";
 
 async function triggerDeploy() {
-  const startTime = new Date().getTime();
+  const tmpDeployFilename = "deploy.tar.gz";
 
-  const packageJson = fs.readFileSync("package.json");
-  const packageJsonContents = JSON.parse(packageJson);
-
-  const { repository } = packageJsonContents.repository;
-
-  // TODO: replace with proper domain
-  const res = await fetch("http://138.197.173.217:4242/deploy", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
+  await tar.create(
+    {
+      gzip: true,
+      file: tmpDeployFilename,
+      filter: (path) => {
+        const excludedPaths = [
+          ".git",
+          ".gitignore",
+          "node_modules",
+          "package.json",
+          "yarn.lock",
+          "package-lock.json",
+          tmpDeployFilename,
+        ];
+        return !excludedPaths.includes(path);
+      },
     },
-    body: JSON.stringify({
-      repository
-    })
-  })
+    ["./"]
+  );
 
-  const endTime = new Date().getTime();
-  const deployTime = endTime - startTime;
+  const readStream = fs.createReadStream("deploy.tar.gz");
+  const form = new FormData();
+  form.append("tar", readStream);
 
-  if (res.status === 200) {
-    console.log(`Deployed in ${deployTime}ms`);
-  }
+  const deployHost = process.env.HOST || "138.197.173.217:4242";
+
+  await axios.post(`http://${deployHost}/deploy/upload`, form, {
+    headers: {
+      ...form.getHeaders(),
+    },
+  });
+
+  await fsPromise.unlink(tmpDeployFilename);
 }
+
+// TODO: Bring back when supporting node deployments
+// async function triggerDeploy() {
+//   const startTime = new Date().getTime();
+
+//   const packageJson = fs.readFileSync("package.json");
+//   const packageJsonContents = JSON.parse(packageJson);
+
+//   const { repository } = packageJsonContents.repository;
+
+//   // TODO: replace with proper domain
+//   const res = await fetch("http://138.197.173.217:4242/deploy", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       repository
+//     })
+//   })
+
+//   const endTime = new Date().getTime();
+//   const deployTime = endTime - startTime;
+
+//   if (res.status === 200) {
+//     console.log(`Deployed in ${deployTime}ms`);
+//   }
+// }
 
 if (process.argv.length >= 3 && process.argv[2] === "deploy") {
   triggerDeploy();
