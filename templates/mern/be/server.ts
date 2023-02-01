e1210688e54d0f0d27fada5ace6c8f99c239fc71
Adam Berg <adam@xyzdigital.com>
2023-02-01 15:54:03 -0800

    Add basic mongodb connection to mern template

--- templates/mern/be/server.ts
+++ templates/mern/be/server.ts
@@ -1,7 +1,33 @@
+import { MongoClient } from "mongodb";
 import express from "express";
+import dotenv from "dotenv";
 
-const app = express();
+async function run() {
+  dotenv.config();
 
-app.use(express.static("./fe/public"));
+  const client = new MongoClient(
+    process.env.DB_URI || "mongodb://127.0.0.1:27017"
+  );
+  await client.connect();
 
-app.listen(process.env.PORT);
+  const db = client.db();
+  const PageView = db.collection("pageviews");
+
+  const app = express();
+
+  app.use(express.static("./fe/public"));
+
+  app.get("/count", async (req, res) => {
+    await PageView.insertOne({
+      path: req.path,
+    });
+
+    const count = await PageView.countDocuments({});
+
+    res.send(`Views: ${count}`);
+  });
+
+  app.listen(process.env.PORT);
+}
+
+run();
