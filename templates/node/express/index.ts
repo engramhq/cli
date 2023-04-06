4daf20a5957ba7312e0b9a646834419c181f305d
Keon Kouhang <Keon.Kouhang@dubsado.com>
2023-04-06 16:06:25 -0400

    Added --dev for local hosting

--- templates/node/express/index.ts
+++ templates/node/express/index.ts
@@ -6,4 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello Express Updated");
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Running on port ${process.env.PORT}`)
+});
